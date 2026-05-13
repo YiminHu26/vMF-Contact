0e3196ea899e4cff4c61ab097dbd4196f46f952e
YiminHu26 <yimin.hu@student.kit.edu>
2026-05-13 11:05:11 +0200

    Added parser for pcd visualizer

--- pcd_visualizer.py
+++ pcd_visualizer.py
@@ -1,4 +1,17 @@
+import argparse
 import open3d as o3d
 
-pcd = o3d.io.read_point_cloud("test_grasp.ply")
+parser = argparse.ArgumentParser(
+    description="Visualize a point cloud file"
+)
+
+parser.add_argument(
+    "file",
+    help="Path to point cloud file"
+)
+
+args = parser.parse_args()
+
+pcd = o3d.io.read_point_cloud(args.file)
+
 o3d.visualization.draw_geometries([pcd])
